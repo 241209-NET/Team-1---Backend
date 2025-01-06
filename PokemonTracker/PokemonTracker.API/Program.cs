a9aa7b4b8689239317e2c61fa12561c0f5aacd05
Rohit12909 <Rohit12909@gmail.com>
2025-01-06 16:27:38 -0500

    Added ignore cycles

--- PokemonTracker/PokemonTracker.API/Program.cs
+++ PokemonTracker/PokemonTracker.API/Program.cs
@@ -23,7 +23,11 @@ builder.Services.AddScoped<IPokemonRepository, PokemonRepository>();
 builder.Services.AddScoped<ITrainerRepository, TrainerRepository>();
 
 // Add Controllers
-builder.Services.AddControllers();
+builder.Services.AddControllers()
+    .AddJsonOptions(options =>
+    {
+        options.JsonSerializerOptions.ReferenceHandler = System.Text.Json.Serialization.ReferenceHandler.IgnoreCycles;
+    });
 
 var app = builder.Build();
 
